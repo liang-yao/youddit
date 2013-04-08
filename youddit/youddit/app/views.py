7abe0d4b91cbe43f2afd8989b50a3e8eaf100ac5
Shamil Ishraq <shamil.ishraq@gmail.com>
2013-04-07 23:32:41 -0400

    Refactored videos and views

--- youddit/youddit/app/views.py
+++ youddit/youddit/app/views.py
@@ -3,26 +3,19 @@ from django.template import RequestContext, loader
 from django.views.generic import View
 from pymongo import MongoClient
 from time import time
-import youddit.videos as Videos, simplejson as json
+from youddit.videos import Videos
+import simplejson as json
 
 def index(request):
-    conn = MongoClient()
-    db = conn.Youddit
-    vids = {}
-    reddit = "videos"
-    ver = db.subreddits.find_one({"name": reddit})['ver'] 
-    v = []
-    for i in db.videos.find({ "subreddit": reddit, "cat": Videos.CATEGORIES['top'], "ver": ver }, {'_id': 0}).sort('pos', 1).limit(25):
-        v.append(i)
+    v = Videos(subreddit='videos')
+    data = v.get_videos('hot', 1, VideosView.LIMIT) 
     template = loader.get_template('index.html')
-    context = RequestContext(request, { "data": json.dumps(v) })
+    context = RequestContext(request, { "data": json.dumps(data) })
     return HttpResponse(template.render(context))
 
 class VideosView(View):
-    # Page size, 100 max
+    # Page size, 100 max, 25 default
     LIMIT = 25
-    subreddit = ''
-    internal_fields = {'_id': 0, 'subreddit': 0, 'cat': 0 , 'pos': 0 , 'ver': 0 }
     
     def dispatch(self, request, subreddit):
         self.subreddit = subreddit
@@ -33,19 +26,20 @@ class VideosView(View):
         if 'page' in request.GET:
             page = int(request.GET['page'])
             if page <= 0:
-                return self.error("Page must be greater than 0", 422)
+                return self._error("Page must be greater than 0", 422)
 
         limit = self.LIMIT
         if 'limit' in request.GET:
             limit = int(request.GET['limit'])
             if limit > 100 or limit <= 0:
-                return self.error("Limit must be between 1 and 100", 422)
+                return self._error("Limit must be between 1 and 100", 422)
 
         cat = "top"
         if 'cat' in request.GET:
             cat = request.GET['cat']
-
-        videos = self.get_videos(cat, page, limit)
+        
+        v = Videos(self.subreddit)
+        videos = v.get_videos(cat, page, limit)
         """
         import pprint
         p = pprint.PrettyPrinter()
@@ -54,37 +48,7 @@ class VideosView(View):
 
         return HttpResponse(json.dumps(videos))
 
-    def get_videos(self, cat, page, limit):
-        db = self.mongo_connect()
-        # Check if we have the subreddit
-        r = db.subreddits.find_one({ "name": self.subreddit })
-        data = {}
-        data["subreddit"] = self.subreddit 
-        data["cat"] = cat
-        data["videos"] = [] 
-
-        if not r:
-            for v in Videos.load_videos(self.subreddit):
-                data['videos'].append(self.remove_internal_fields(v))
-        else:
-            query = db.videos.find({ "subreddit": self.subreddit, 
-                                      "cat": Videos.CATEGORIES[cat], 
-                                      "ver": r['ver'] }, 
-                                      self.internal_fields).sort('pos', 1).skip((page-1)*limit).limit(limit)
-            data['videos'] = [ v for v in query ]
-                
-        return data
-
-    def remove_internal_fields(self, source):
-        for f in self.internal_fields:
-            del source[f]
-        return source
-
-    def mongo_connect(self):
-        c = MongoClient()
-        return c.Youddit
-
-    def error(self, msg, code):
+    def _error(self, msg, code):
         err = { "error": msg, "status": code }
         response = HttpResponse(json.dumps(err))
         response.status = code
--- youddit/youddit/videos.py
+++ youddit/youddit/videos.py
@@ -1,142 +1,172 @@
 import urllib2, urllib, urlparse, simplejson as json, time, re, sys
 from pymongo import MongoClient
 
-PROVIDERS = { "youtube.com": 1, "youtu.be": 2, "vimeo.com": 3 }
-CATEGORIES = { "top": 0, "hot": 1, "controversial": 2 }
-# List of subreddits we update per hour
-COOL_REDDITS = ["videos"]
+class Videos():
+    PROVIDERS = { "youtube.com": 1, "youtu.be": 2, "vimeo.com": 3 }
+    CATEGORIES = { "top": 0, "hot": 1, "controversial": 2 }
+    # List of subreddits we update per hour
+    COOL_REDDITS = ["videos"]
+    INTERNAL_FIELDS = {'_id': 0, 'subreddit': 0, 'cat': 0 , 'pos': 0 , 'ver': 0 }
 
-# Mongo
-conn = MongoClient()
-db = conn.Youddit
 
-def get_videos(subreddit, category, **kwargs):
-    url = "http://www.reddit.com/r/%s/%s.json"%(subreddit, category)
-    pages = 30
-    if "pages" in kwargs:
-        pages = kwargs["pages"]
-    videos = []
+    def __init__(self, **kwargs):
+        self.subreddit = kwargs.get('subreddit', '')
+        self.conn = MongoClient()
+        self.db = self.conn.Youddit
 
-    after = ''
-    position = 0
-    for page in range(0, pages):
-        response = request(url, {'limit':100, 'after': after})
-        if len(response['data']['children']) == 0:
-            break
-        for reddit in response['data']['children']:
-            if reddit['data']['domain'] in PROVIDERS:
-                provider = PROVIDERS[reddit['data']['domain']] 
+    def load_videos(self, r):
+        print "Subreddit: ", r
+        reddit = self.db.subreddits.find_one({"name": r})
+        if reddit:
+            ver = reddit['ver']
+        else:
+            ver = 0
+        ver += 1
+        videos = []
+        for cat in self.CATEGORIES:
+            print cat
+            videos = self._get_videos(r, cat, pages=30)
+            # Add version and category to each video in list
+            videos = [ self._merge(v, {"ver": ver, "cat": self.CATEGORIES[cat]}) for v in videos ]
+            self.db.videos.insert(videos)
+            self.db.subreddits.update({ "name": r }, { "name": r,
+                                                  "updated_at": time.time(),
+                                                  "ver": ver
+                                                }, True )
+        return videos
+
+    def get_videos(self, cat, page, limit):
+        # Check if we have the subreddit
+        r = self.db.subreddits.find_one({ "name": self.subreddit })
+        data = {}
+        data["subreddit"] = self.subreddit 
+        data["cat"] = cat
+        data["videos"] = [] 
+
+        if not r:
+            for v in self.load_videos(self.subreddit):
+                data['videos'].append(self._remove_internal_fields(v))
+        else:
+            query = self.db.videos.find({ "subreddit": self.subreddit, 
+                                      "cat": self.CATEGORIES[cat], 
+                                      "ver": r['ver'] }, 
+                                      self.INTERNAL_FIELDS).sort('pos', 1).skip((page-1)*limit).limit(limit)
+            data['videos'] = [ v for v in query ]
                 
-                # Remove vimeo for this version
-                if provider == PROVIDERS["vimeo.com"]:
-                    continue
+        return data
 
-                r = reddit['data']
-                vid = _get_vid(provider, r['url'])
-                # Some videos dont have proper links, so we skip them
-                if vid == '':
-                    continue
-                position += 1
-                # appending the video data to the videos list
-                videos.append({ "rid": r['id'], # Reddit id
-                                "title": r['title'],
-                                "provider": provider, 
-                                "vid": vid, 
-                                "subreddit": r['subreddit'],
-                                "score": r['score'],
-                                "permalink": r['permalink'],
-                                "created": r['created'],
-                                "pos": position
-                              })
+    def _remove_internal_fields(self, source):
+        for f in self.INTERNAL_FIELDS:
+            del source[f]
+        return source
 
-        after = response['data']['after']
-        print "After: ", after
-        if not after:
-            break
-        time.sleep(1)
-    return videos
+    def _get_videos(self, subreddit, category, **kwargs):
+        url = "http://www.reddit.com/r/%s/%s.json"%(subreddit, category)
+        pages = 30
+        if "pages" in kwargs:
+            pages = kwargs["pages"]
+        videos = []
 
-def request(url, params):
-    params = urllib.urlencode(params)
-    req = urllib2.Request(url + '?' + params)
-    req.add_header('User-Agent', 'reddit video stream youddit')
-    req.add_header('Content-Type', 'application/json')
-   
-    try:
-        response = urllib2.urlopen(req)
-    except Exception as e:
-        print e
-        time.sleep(5)
-        return request(url, params)
-        
-    response = json.loads(response.read())
-    return response
-  
-# Returns the video id 
-def _get_vid(provider, url):
-    u = urlparse.urlparse(url)
-    if provider == PROVIDERS['youtube.com']:
+        after = ''
+        position = 0
+        for page in range(0, pages):
+            response = self._request(url, {'limit':100, 'after': after})
+            if len(response['data']['children']) == 0:
+                break
+            for reddit in response['data']['children']:
+                if reddit['data']['domain'] in self.PROVIDERS:
+                    provider = self.PROVIDERS[reddit['data']['domain']] 
+                    
+                    # Remove vimeo for this version
+                    if provider == self.PROVIDERS["vimeo.com"]:
+                        continue
+
+                    r = reddit['data']
+                    vid = self._get_vid(provider, r['url'])
+                    # Some videos dont have proper links, so we skip them
+                    if vid == '':
+                        continue
+                    position += 1
+                    # appending the video data to the videos list
+                    videos.append({ "rid": r['id'], # Reddit id
+                                    "title": r['title'],
+                                    "provider": provider, 
+                                    "vid": vid, 
+                                    "subreddit": r['subreddit'],
+                                    "score": r['score'],
+                                    "permalink": r['permalink'],
+                                    "created": r['created'],
+                                    "pos": position
+                                  })
+
+            after = response['data']['after']
+            print "After: ", after
+            if not after:
+                break
+            time.sleep(1)
+        return videos
+
+    def _request(self, url, params):
+        params = urllib.urlencode(params)
+        req = urllib2.Request(url + '?' + params)
+        req.add_header('User-Agent', 'reddit video stream youddit')
+        req.add_header('Content-Type', 'application/json')
+       
         try:
-            return urlparse.parse_qs(u.query)['v'][0]
-        except KeyError:
+            response = urllib2.urlopen(req)
+        except Exception as e:
+            print e
+            time.sleep(5)
+            return request(url, params)
+            
+        response = json.loads(response.read())
+        return response
+      
+    # Returns the video id 
+    def _get_vid(self, provider, url):
+        u = urlparse.urlparse(url)
+        if provider == self.PROVIDERS['youtube.com']:
+            try:
+                return urlparse.parse_qs(u.query)['v'][0]
+            except KeyError:
+                return ''
+        elif provider == self.PROVIDERS['youtu.be'] or provider == self.PROVIDERS['vimeo.com']:
+            return u.path[1:]
+
+    # Return the thumbnail url if its there
+    def _get_thumbnail(self, data):
+        if "thumbnail" in data:
+            return data['thumbnail']
+        else:
             return ''
-    elif provider == PROVIDERS['youtu.be'] or provider == PROVIDERS['vimeo.com']:
-        return u.path[1:]
 
-# Return the thumbnail url if its there
-def _get_thumbnail(data):
-    if "thumbnail" in data:
-        return data['thumbnail']
-    else:
-        return ''
+    def _merge(self, d1, d2):
+        d1.update(d2)
+        return d1
 
-def load_videos(r):
-    print "Subreddit: ", r
-    reddit = db.subreddits.find_one({"name": r})
-    if reddit:
-        ver = reddit['ver']
-    else:
-        ver = 0
-    ver += 1
-    videos = []
-    for cat in CATEGORIES:
-        print cat
-        videos = get_videos(r, cat, pages=30)
-        # Add version and category to each video in list
-        videos = [ merge(v, {"ver": ver, "cat": CATEGORIES[cat]}) for v in videos ]
-        db.videos.insert(videos)
-        db.subreddits.update({ "name": r }, { "name": r,
-                                              "updated_at": time.time(),
-                                              "ver": ver
-                                            }, True )
-    return videos
+    # Update the cool reddits
+    # This should be called in a cron job
+    @staticmethod
+    def update():
+        v =  Videos()
+        for r in Videos.COOL_REDDITS:
+            v.load_videos(r)
 
-# Update the cool reddits
-# This should be called in a cron job
-def update():
-    for r in COOL_REDDITS:
-        load_videos(r)
-
-def clean_up():
-    conn = MongoClient()
-    db = conn.Youddit
-    for r in COOL_REDDITS:
-        reddit = db.subreddits.find_one({"name": r})
-        print r
-        if not reddit:
-            continue
-        db.videos.remove({"subreddit": reddit['name'], "ver": { "$lt": reddit['ver'] }}) 
-    conn.close()
-
-def merge(d1, d2):
-    d1.update(d2)
-    return d1
+    @staticmethod
+    def clean_up():
+        db = MongoClient().Youddit
+        for r in Videos.COOL_REDDITS:
+            reddit = db.subreddits.find_one({"name": r})
+            print r
+            if not reddit:
+                continue
+            db.videos.remove({"subreddit": reddit['name'], "ver": { "$lt": reddit['ver'] }}) 
 
 if __name__ == '__main__':
     cmd = sys.argv[1]
     if cmd == 'update':
-        update()
-        clean_up()
+        Videos.update()
+        Videos.clean_up()
         print 'Done'
 
 
